c2cd4ce838d47f7519521a1208aec6d9ac965d00
Rob08rua <rcarlos_ruamtz.08@outlook.com>
2024-12-16 02:33:18 -0600

    Update Delete.php

--- Urbina/Vista/Admin/Delete.php
+++ Urbina/Vista/Admin/Delete.php
@@ -5,24 +5,27 @@ session_start();
 
 // Verificar si el administrador está logueado
 if (!isset($_SESSION['admin_id'])) {
-    //header("Location: ../Vista/Login_Admin.php");
-    header("Login_Admin.php");
+    header("Location: ../Vista/Admin/Login_Admin.php");
+    //header("Login_Admin.php");
     exit();
 }
 
-//include('../Controlador/Conexion_BD.php');
-include('conexion.php');
+include('../Controlador/Conexion_BD.php');
+//include('conexion.php');
 
 // Verificar si se ha recibido el ID del producto
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
     // Eliminar el producto
-    $sql = "UPDATE productos SET activo = 0 WHERE id = ?";
-    $stmt = $conn->prepare($sql);
+    $sql = "UPDATE Producto SET activo = 0 WHERE id = ?";
+    //$sql = "UPDATE productos SET activo = 0 WHERE id = ?";
+    //$stmt = $conn->prepare($sql);
+    $stmt = $pdo->prepare($sql);
     $stmt->execute([$id]);
 
-    header("Location: Admin.php");
+    header("Location: ../Vista/Admin/Admin.php");
+    //header("Location: Admin.php");
     exit();
 } else {
     echo "Error: No se ha proporcionado el ID del producto.";
